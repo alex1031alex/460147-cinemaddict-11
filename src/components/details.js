01e111453a0e5c1e396ada4b05c037c01d322d8c
Alex <alex1031alex@yandex.ru>
2020-04-04 17:39:34 +0300

    Разбивает код на модули, создает отдельные файлы для каждого модуля

--- src/components/details.js
+++ src/components/details.js
@@ -1,25 +1,3 @@
-import {createUserProfileTemplate} from './components/profile.js';
-import {createMenuTemplate} from './components/menu.js';
-import {createSortingTemplate} from './components/sorting.js';
-import {createMovieBoardTemplate} from './components/board.js';
-import {createMovieCardTemplate} from './components/card.js';
-import {createShowMoreButtonTemplate} from './components/show-more-button.js';
-
-const MAIN_MOVIE_COUNT = 5;
-const EXTRA_MOVIE_COUNT = 2;
-const headerElement = document.querySelector(`.header`);
-const mainElement = document.querySelector(`.main`);
-
-const render = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template);
-};
-
-const renderList = (container, template, count) => {
-  for (let i = 0; i < count; i++) {
-    render(container, template);
-  }
-};
-
 const createMovieDetailsTemplate = () => (
   `<section class="film-details">
      <form class="film-details__inner" action="" method="get">
@@ -242,20 +220,4 @@ const createMovieDetailsTemplate = () => (
    </section>`
 );
 
-render(headerElement, createUserProfileTemplate());
-render(mainElement, createMenuTemplate());
-render(mainElement, createSortingTemplate());
-render(mainElement, createMovieBoardTemplate());
-
-const mainMovieListElement = mainElement.querySelector(`.films-list .films-list__container`);
-
-renderList(mainMovieListElement, createMovieCardTemplate(), MAIN_MOVIE_COUNT);
-render(mainMovieListElement, createShowMoreButtonTemplate(), `afterend`);
-
-const extraMovieListElements = mainElement.querySelectorAll(`.films-list--extra .films-list__container`);
-
-for (let element of extraMovieListElements) {
-  renderList(element, createMovieCardTemplate(), EXTRA_MOVIE_COUNT);
-}
-
-// render(mainElement, createMovieDetailsTemplate());
+export {createMovieDetailsTemplate};
