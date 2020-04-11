b556c8361b90b312f570c97311a8b49faf9f7a07
Alex <alex1031alex@yandex.ru>
2020-04-11 20:59:34 +0300

    Исправляет логику генерации постеров

--- src/mock/film.js
+++ src/mock/film.js
@@ -2,29 +2,32 @@ const getRandomNumber = (min, max) => {
   return Math.floor(min + Math.random() * (max - min + 1));
 };
 
+const getRandomArrayItem = (array) => {
+  return array[getRandomNumber(0, array.length - 1)];
+};
+
 const FILM_NAMES = [
-  `Made for each other`,
-  `Popeye meets Sinbad`,
-  `Sagebrush trail`,
-  `Santa Claus conqers the martians`,
-  `The dance of life`,
-  `The great Flamarion`,
-  `The man with the golden arm`,
+  `Made for Each Other`,
+  `Popeye the Sailor meets Sinbad the Sailor`,
+  `Sagebrush Trail`,
+  `Santa Claus Conqers the Martians`,
+  `The Dance of Life`,
+  `The Great Flamarion`,
+  `The Man with the Golden Arm`,
+];
+
+const POSTERS = [
+  `made-of-each-other.png`,
+  `popeye-meets-sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`,
 ];
 
 const DESCRIPTION_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 const SENTENCE_LIMIT = 5;
-
-const generatePoster = (filmName) => {
-  const posterName = filmName.toLowerCase().replace(/\s/g, `-`);
-  switch(filmName) {
-    case (`Made for each other`):
-    case (`Popeye meets Sinbad`):
-      return `${posterName}.png`;
-    default: 
-      return `${posterName}.jpg`;
-  }
-};
 
 const generateRandomDesc = (text, limit) => {
   const descriptionItems = text.split(`.`);
@@ -38,8 +41,8 @@ const generateRandomDesc = (text, limit) => {
 }; 
 
 const generateFilm = () => {
-  const name = FILM_NAMES[getRandomNumber(0, FILM_NAMES.length)];
-  const poster = generatePoster(name);
+  const name = getRandomArrayItem(FILM_NAMES);
+  const poster = getRandomArrayItem(POSTERS);
   const description = generateRandomDesc(DESCRIPTION_TEXT, SENTENCE_LIMIT);
   return {
     name,
