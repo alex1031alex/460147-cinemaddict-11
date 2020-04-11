03d0355e04f16ffe2b8b58156fd1008e779b8e20
Alex <alex1031alex@yandex.ru>
2020-04-11 15:18:12 +0300

    Создаёт массив названий фильмов

--- src/mock/film.js
+++ src/mock/film.js
@@ -2,6 +2,16 @@ const getRandomNumber = (min, max) => {
   return Math.floor(min + Math.random() * (max - min + 1));
 };
 
+const FILM_NAMES = [
+  `Made for each other`,
+  `Popeye meets Sinbad`,
+  `Sagebrush trail`,
+  `Santa Claus conqers the martians`,
+  `The dance of life`,
+  `The great Flamarion`,
+  `The man with the golden arm`,
+];
+
 const generateFilm = () => {
   return {
     name: null,
