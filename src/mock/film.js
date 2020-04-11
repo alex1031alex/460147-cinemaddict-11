0f80ef57309610f9451b3226fa48ab4adb7de0c0
Alex <alex1031alex@yandex.ru>
2020-04-11 14:51:57 +0300

    Создаёт функции для генерации данных для фильма

---
+++ src/mock/film.js
@@ -0,0 +1,13 @@
+const generateFilm = () => {
+  return {
+    name: null,
+    poster: null,
+    description: null,
+    comment: {},
+  }
+};
+
+const generateFilms = () => {
+  const films = [];
+  return films;
+};
