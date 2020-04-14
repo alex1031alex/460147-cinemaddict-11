8f35d07a98aa485085c40a46d4f355a89f39fc40
Alex <alex1031alex@yandex.ru>
2020-04-14 15:54:54 +0300

    Добавляет классы для кнопок сортировки

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,13 +1,13 @@
 const createSortingTemplate = () => (
   `<ul class="sort">
      <li>
-       <a href="#" class="sort__button sort__button--active">Sort by default</a>
+       <a href="#" class="sort__button sort__button--default sort__button--active">Sort by default</a>
      </li>
      <li>
-       <a href="#" class="sort__button">Sort by date</a>
+       <a href="#" class="sort__button sort__button--date">Sort by date</a>
      </li>
      <li>
-       <a href="#" class="sort__button">Sort by rating</a>
+       <a href="#" class="sort__button sort__button--rating">Sort by rating</a>
      </li>
    </ul>`
 );
