98812d53f701e037c0b04c6b192cc6d863b45b44
Alex <alex1031alex@yandex.ru>
2020-04-15 22:03:56 +0300

    Убирает пробелы по краям

--- src/mock/profile.js
+++ src/mock/profile.js
@@ -1,16 +1,16 @@
 const getUserTitle = (watchedFilms) => {
   if (watchedFilms === 0) {
     return ``;
-  } 
-  
+  }
+
   if (watchedFilms < 11) {
     return `novice`;
-  } 
-  
+  }
+
   if (watchedFilms < 21) {
     return `fan`;
   }
-  
+
   return `movie buff`;
 };
 
