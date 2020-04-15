8bd32d4a05347ca7573e7eb5b77a7072a1fd3562
Alex <alex1031alex@yandex.ru>
2020-04-15 22:02:46 +0300

    Исправляет ошибку в отрисовке значения фильтров

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,7 @@
 const createFilterMarkup = (filter, isActive) => {
   const {name, count} = filter;
   const activeClass = isActive ? `main-navigation__item--active` : ``;
-  const countToShow = count > 5 ? `` : `<span class="main-navigation__item-count">${count}</span>`;
+  const countToShow = count > 5 || count === 0 ? `` : `<span class="main-navigation__item-count">${count}</span>`;
   return (
     `<a href="#${name}" class="main-navigation__item ${activeClass}">
       ${name}${countToShow}
