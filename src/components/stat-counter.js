46f8a9410853667bd455cca23b3d3d40710dbb6d
Alex <alex1031alex@yandex.ru>
2020-04-20 17:25:21 +0300

    Переписывает на классы остальные компоненты

--- src/components/stat-counter.js
+++ src/components/stat-counter.js
@@ -1,3 +1,23 @@
+import {createElement} from './utils.js';
+
 const createStatCounterTemplate = (films) => `<p>${films.length} movies inside</p>`;
 
-export {createStatCounterTemplate};
+export default class StatCounter {
+  constructor() {
+    this._films = films;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createStatCounterTemplate(this._films);
+  }
+
+  getElement() {
+    createElement(this.getTemplate());
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+}
--- src/components/stat.js
+++ src/components/stat.js
@@ -1,3 +1,5 @@
+import {createElement} from './utils.js';
+
 const creatStatTemplate = () => (
   `<section class="statistic">
     <p class="statistic__rank">
@@ -82,4 +84,24 @@ const creatStatTemplate = () => (
   </section>`
 );
 
-export {creatStatTemplate};
+export default class Stat {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return creatStatTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
