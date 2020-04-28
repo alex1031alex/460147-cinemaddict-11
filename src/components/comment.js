b540fb43f2151587ac6f841bef456a66d7cd2944
Alex <alex1031alex@yandex.ru>
2020-04-28 09:25:23 +0300

    Наследует класс comment от абстрактного компонента

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,4 +1,4 @@
-import {formatDateTime, createElement} from './utils.js';
+import AbstractComponent from './abstract-component.js';
 
 const createCommentMarkup = (comment) => {
   const {emoji, date, author, message} = comment;
@@ -27,25 +27,13 @@ const createCommentMarkup = (comment) => {
 //     .join(`\n`);
 // };
 
-export default class Comment {
+export default class Comment extends AbstractComponent {
   constructor(comment) {
+    super();
     this._comment = comment;
-    this._element = null;
   }
 
   getTemplate() {
     return createCommentMarkup(this._comment);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
