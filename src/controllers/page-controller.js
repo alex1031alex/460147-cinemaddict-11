f5821dccc13dc1148be1bc086e1ddc4f2867db57
Alex <alex1031alex@yandex.ru>
2020-05-02 16:39:34 +0300

    Выносит отрисовку кнопки в отдельный приватный метод

--- src/controllers/page-controller.js
+++ src/controllers/page-controller.js
@@ -9,6 +9,7 @@ let movieShowingCount = INITIAL_MOVIE_COUNT;
 
 export default class PageController {
   constructor(boardComponent) {
+    this._movies = [];
     this._board = boardComponent;
     this._ratedMovieElement = boardComponent.getRatedMovieElement();
     this._commentMovieElement = boardComponent.getCommentMovieElement();
@@ -24,30 +25,38 @@ export default class PageController {
     });
   }
 
-  render(movies) {
-    const topRatedShowingFilms = movies
-      .sort((a, b) => b.rating - a.rating)
-      .slice(0, EXTRA_MOVIE_COUNT);
-    const mostCommentedShowingFilms = movies
-      .sort((a, b) => b.comments.length - a.comments.length)
-      .slice(0, EXTRA_MOVIE_COUNT);
-    const mainShowingFilms = movies.slice(0, INITIAL_MOVIE_COUNT);
-
-    this._renderFilmCards(this._ratedMovieElement, topRatedShowingFilms);
-    this._renderFilmCards(this._commentMovieElement, mostCommentedShowingFilms);
-    this._renderFilmCards(this._mainMovieElement, mainShowingFilms);
+  _renderShowMoreButton() {
+    if (movieShowingCount >= this._movies.length) {
+      return;
+    }
 
     render(this._mainMovieElement, this._showMoreButtonComponent, `afterend`);
 
     this._showMoreButtonComponent.setClickHandler(() => {
       const prevMovieCount = movieShowingCount;
       movieShowingCount += MOVIE_COUNT_BY_BUTTON;
-      const showingFilms = movies.slice(prevMovieCount, movieShowingCount);
+      const showingFilms = this._movies.slice(prevMovieCount, movieShowingCount);
       this._renderFilmCards(this._mainMovieElement, showingFilms);
 
-      if (movieShowingCount >= movies.length) {
+      if (movieShowingCount >= this._movies.length) {
         removeComponent(this._showMoreButtonComponent);
       }
     });
+  }
+
+  render(movies) {
+    this._movies = movies;
+    const topRatedShowingFilms = this._movies
+      .sort((a, b) => b.rating - a.rating)
+      .slice(0, EXTRA_MOVIE_COUNT);
+    const mostCommentedShowingFilms = this._movies
+      .sort((a, b) => b.comments.length - a.comments.length)
+      .slice(0, EXTRA_MOVIE_COUNT);
+    const mainShowingFilms = this._movies.slice(0, INITIAL_MOVIE_COUNT);
+
+    this._renderFilmCards(this._ratedMovieElement, topRatedShowingFilms);
+    this._renderFilmCards(this._commentMovieElement, mostCommentedShowingFilms);
+    this._renderFilmCards(this._mainMovieElement, mainShowingFilms);
+    this._renderShowMoreButton();
   }
 }
