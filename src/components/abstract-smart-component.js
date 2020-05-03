4a20b0565f28f5a641c79cf4b335095e992c4e2f
Alex <alex1031alex@yandex.ru>
2020-05-03 13:56:37 +0300

    Делает невозможным создание объекта из абстрактного класса напрямую. Делает метод recoveryListeners абстрактным

--- src/components/abstract-smart-component.js
+++ src/components/abstract-smart-component.js
@@ -1,11 +1,18 @@
 import AbstractComponent from './abstract-component.js';
 
 export default class AbstractSmartComponent extends AbstractComponent {
-  recoveryListeners() {
+  constructor() {
+    super();
+    if (new.target === AbstractSmartComponent) {
+      throw new Error(`Can't instantiate Abstract Smart Component, only concrete one.`);
+    }
+  }
 
+  recoveryListeners() {
+    throw new Error(`Abstract method not implemented: recoveryListeners`);
   }
 
   rerender() {
-    
+
   }
 }
