4de0e200a0ea74ecacf56aaa18883577b14809e8
Alex <alex1031alex@yandex.ru>
2020-05-03 16:15:45 +0300

    Реализует метод rerender в абстрактном компоненте

--- src/components/abstract-smart-component.js
+++ src/components/abstract-smart-component.js
@@ -13,6 +13,15 @@ export default class AbstractSmartComponent extends AbstractComponent {
   }
 
   rerender() {
+    const oldElement = this.getElement();
+    const parentElement = oldElement.parentElement;
 
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parentElement.replaceChild(newElement, oldElement);
+
+    this.recoveryListeners();
   }
 }
