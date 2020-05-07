8d0f2f41b130818b562ab503bc9fb5b263d4fc80
Alex <alex1031alex@yandex.ru>
2020-05-08 01:13:55 +0300

    Удаляет пустую строку

--- src/components/abstract-smart-component.js
+++ src/components/abstract-smart-component.js
@@ -3,6 +3,7 @@ import AbstractComponent from './abstract-component.js';
 export default class AbstractSmartComponent extends AbstractComponent {
   constructor() {
     super();
+
     if (new.target === AbstractSmartComponent) {
       throw new Error(`Can't instantiate Abstract Smart Component, only concrete one.`);
     }
