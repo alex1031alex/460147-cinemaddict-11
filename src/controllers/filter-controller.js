65a55c8e64fce38251e26e26ff182f40ac448582
Alex <alex1031alex@yandex.ru>
2020-05-11 22:58:44 +0300

    Реализует фильтрацию в зависимости от типа фильтра

--- src/controllers/filter-controller.js
+++ src/controllers/filter-controller.js
@@ -8,7 +8,18 @@ const FilterType = {
   FAVORITES: `Favorites`,
 };
 
-const getMoviesByFilter = (movies, filterType) => movies;
+const getMoviesByFilter = (movies, filterType) => {
+  switch (filterType) {
+    case FilterType.ALL_MOVIES:
+      return movies;
+    case FilterType.WATCHLIST:
+      return movies.filter((movie) => movie.isAtWatchlist);
+    case FilterType.HISTORY:
+      return movies.filter((movie) => movie.isWatched);
+    case FilterType.FAVORITES:
+      return movies.filter((movie) => movie.isFavorite);
+  }
+};
 
 export default class FilterController {
   constructor(container, moviesModel) {
