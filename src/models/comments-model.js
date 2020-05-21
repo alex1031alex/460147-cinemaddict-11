f0e5f9ed5ebdcdd8d26867d92b372c6bcbfc33fe
Alex <alex1031alex@yandex.ru>
2020-05-21 20:40:12 +0300

    Описывает методы добавления и удаления комментариев

--- src/models/comments-model.js
+++ src/models/comments-model.js
@@ -15,8 +15,22 @@ export default class CommentsModel {
     this._callHandlers(this._dataChangeHandlers);
   }
 
-  addComment() {
+  addComment(comment) {
+    this._comments.push(comment);
+    this._callHandlers(this._dataChangeHandlers);
+  }
 
+  deleteComment(id) {
+    const index = this._comments.findIndex((comment) => comment.id === id );
+
+    if (index === -1) {
+      return false;
+    }
+
+    this._comments = [].concat(this._comments.slice(0, index), this._comments.slice(index + 1));
+    this._callHandlers(this._dataChangeHandlers);
+
+    return true;
   }
 
   setDataChangeHandler(handler) {
