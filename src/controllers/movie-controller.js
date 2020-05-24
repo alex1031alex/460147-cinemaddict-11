25a54a9558bf74dc7083020edcece01e1f06bb7c
Alex <alex1031alex@yandex.ru>
2020-05-24 16:59:04 +0300

    Удаляет пробелы по краям

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -77,7 +77,7 @@ export default class MovieController {
           emoji: emojiType,
           message: commentText,
         };
-  
+
         this._commentsModel.addComment(comment);
       }
     }
