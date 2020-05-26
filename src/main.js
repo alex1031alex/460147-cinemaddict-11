ea2cf0f99f34ba9dee25b000dea934e632a54fef
Alex <alex1031alex@yandex.ru>
2020-05-26 12:40:49 +0300

    Удаляет комментарии в main.js

--- src/main.js
+++ src/main.js
@@ -13,38 +13,31 @@ import FilterController from './controllers/filter-controller.js';
 
 const TOTAL_MOVIE_COUNT = 20;
 
-/* Элементы страницы */
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
 const footer = document.querySelector(`.footer__statistics`);
 
-/* Данные */
 const films = generateFilms(TOTAL_MOVIE_COUNT);
 
 const watchedFilms = films.filter((film) => film.isWatched).length;
 
-/* Создание модели */
 const moviesModel = new MoviesModel();
 
 moviesModel.setMovies(films);
 
-/* Отрисовка меню и профиля пользователя */
 const menuComponent = new MenuComponent();
 
 render(header, new ProfileComponent(getUserTitle(watchedFilms)));
 render(main, menuComponent);
 
-/* Отрисовка фильтров */
 const filterContainer = menuComponent.getElement();
 const filterController = new FilterController(filterContainer, moviesModel);
 filterController.render();
 
-/* Отрисовка сотрировки */
 const sortingComponent = new SortingComponent();
 
 render(main, sortingComponent);
 
-/* Отрисовка доски со списками фильмов */
 if (films.length > 0) {
   const boardComponent = new BoardComponent();
   const pageController = new PageController(boardComponent, moviesModel);
@@ -57,5 +50,4 @@ if (films.length > 0) {
   render(main, noFilmsComponent);
 }
 
-/* Отрисовка счётчика в подвале страницы */
 render(footer, new StatCounterComponent(moviesModel.getMovies().length));
