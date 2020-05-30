e5a47e1c658414fcc381567650fce252f36f7b7c
Alex <alex1031alex@yandex.ru>
2020-05-30 17:49:28 +0300

    Создаёт метод для получения комментариев с сервера

--- src/api.js
+++ src/api.js
@@ -10,7 +10,20 @@ export default class API {
     headers.append(`Authorization`, this._authorization);
 
     return fetch(`https://11.ecmascript.pages.academy/cinemaddict/movies`, {headers})
-      .then((response) => response.json())
+      .then((response) => {
+        const resp = response.json();
+        // console.log(resp);
+        return resp;
+      })
       .then(MovieModel.parseMovies);
+  }
+
+  getComments(movieId) {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`https://11.ecmascript.pages.academy/cinemaddict/comments/${movieId}`, { headers })
+      .then((response) => response.json());
+    
   }
 }
