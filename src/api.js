3274a806b4a5ffb6ae8f4e4ba509cda6aa788c6d
Alex <alex1031alex@yandex.ru>
2020-06-01 21:09:54 +0300

    Исправляет ошибку в методе проверки статуса запроса

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import MovieModel from './models/movie-model.js';
 import {Method} from './const.js';
 
 const checkStatus = (response) => {
-  if (response.status >= 200 && response.status <= 300) {
+  if (response.status >= 200 && response.status < 300) {
     return response;
   } else {
     throw new Error(`${response.status}: ${response.statusText}`);
