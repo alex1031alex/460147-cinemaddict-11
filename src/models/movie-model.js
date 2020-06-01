7e8bf93021970eeb75c290fd3fa67f14fa8c599d
Alex <alex1031alex@yandex.ru>
2020-06-01 16:15:27 +0300

    Делает единообразным написание свойств объектов

--- src/models/movie-model.js
+++ src/models/movie-model.js
@@ -1,54 +1,54 @@
 export default class MovieModel {
   constructor(data) {
-    this.details = {};
-    this.id = data[`id`];
-    this.name = data[`film_info`][`title`];
-    this.poster = data[`film_info`].poster;
-    this.description = data[`film_info`].description;
-    this.runtime = data[`film_info`].runtime;
-    this.genres = data[`film_info`].genre;
-    this.rating = data[`film_info`][`total_rating`];
-    this.comments = data.comments;
-    this.isAtWatchlist = data[`user_details`].watchlist;
-    this.isWatched = data[`user_details`][`already_watched`];
-    this.isFavorite = data[`user_details`].favorite;
-    this.details.ageRating = data[`film_info`][`age_rating`];
-    this.details.originTitle = data[`film_info`][`alternative_title`];
-    this.details.releaseDate = new Date(data[`film_info`].release.date);
-    this.year = this.details.releaseDate.getFullYear();
-    this.details.country = data[`film_info`][`release`][`release_country`];
-    this.details.director = data[`film_info`].director;
-    this.details.writers = data[`film_info`].writers;
-    this.details.actors = data[`film_info`].actors;
-    this.watchingDate = data[`user_details`][`watching_date`];
+    this[`details`] = {};
+    this[`id`] = data[`id`];
+    this[`name`] = data[`film_info`][`title`];
+    this[`poster`] = data[`film_info`][`poster`];
+    this[`description`] = data[`film_info`][`description`];
+    this[`runtime`] = data[`film_info`][`runtime`];
+    this[`genres`] = data[`film_info`][`genre`];
+    this[`rating`] = data[`film_info`][`total_rating`];
+    this[`comments`] = data[`comments`];
+    this[`isAtWatchlist`] = data[`user_details`][`watchlist`];
+    this[`isWatched`] = data[`user_details`][`already_watched`];
+    this[`isFavorite`] = data[`user_details`][`favorite`];
+    this[`details.ageRating`] = data[`film_info`][`age_rating`];
+    this[`details.originTitle`] = data[`film_info`][`alternative_title`];
+    this[`details.releaseDate`] = new Date(data[`film_info`][`release.date`]);
+    this[`year`] = this[`details`][`releaseDate`].getFullYear();
+    this[`details.country`] = data[`film_info`][`release`][`release_country`];
+    this[`details.director`] = data[`film_info`][`director`];
+    this[`details.writers`] = data[`film_info`][`writers`];
+    this[`details.actors`] = data[`film_info`][`actors`];
+    this[`watchingDate`] = data[`user_details`][`watching_date`];
   }
 
   toRAW() {
     return {
-      id: this.id,
-      comments: this.comments,
+      [`id`]: this[`id`],
+      [`comments`]: this[`comments`],
       [`film_info`]: {
-        actors: this.details.actors,
-        writers: this.details.writers,
-        [`age_rating`]: this.details.ageRating,
-        [`alternative_title`]: this.details.originTitle,
-        description: this.description,
-        director: this.details.director,
-        genre: this.genres,
-        poster: this.poster,
-        release: {
-          date: this.details.releaseDate.toISOString(),
-          [`release_country`]: this.details.country,
+        [`actors`]: this[`details`].[`actors`],
+        [`writers`]: this[`details`][`writers`],
+        [`age_rating`]: this[`details`][`ageRating`],
+        [`alternative_title`]: this[`details`][`originTitle`],
+        [`description`]: this[`description`],
+        [`director`]: this[`details`][`director`],
+        [`genre`]: this[`genres`],
+        [`poster`]: this[`poster`],
+        [`release`]: {
+          [`date`]: this.[`details`][`releaseDate`].toISOString(),
+          [`release_country`]: this[`details`][`country`],
         },
-        runtime: this.runtime,
-        title: this.name,
-        [`total_rating`]: this.rating,
+        [`runtime`]: this[`runtime`],
+        [`title`]: this[`name`],
+        [`total_rating`]: this[`rating`],
       },
       [`user_details`]: {
-        watchlist: this.isAtWatchlist,
-        [`already_watched`]: this.isWatched,
-        favorite: this.isFavorite,
-        [`watching_date`]: this.watchingDate,
+        [`watchlist`]: this[`isAtWatchlist`],
+        [`already_watched`]: this[`isWatched`],
+        [`favorite`]: this[`isFavorite`],
+        [`watching_date`]: this[`watchingDate`],
       }
     };
   }
